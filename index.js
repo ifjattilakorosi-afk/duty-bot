87fa75c009d0709304377baba01fc9695c219263
ifjattilakorosi-afk <ifj.attila.korosi@gmail.com>
2026-04-30 18:20:58 +0200

    index.js

--- index.js
+++ index.js
@@ -242,4 +242,4 @@ client.on(Events.InteractionCreate, async (interaction) => {
 });
 
 // =====================
-client.login("MTQ5OTA0MTA4NjU3ODg4NDYwOA.GSUfUJ.QwPXLhLohbeyjPR4p-GBy-swiap13pVIkfFP0I");
+client.login("MTQ5OTA0MTA4NjU3ODg4NDYwOA.GWlEee.n7An4p3kamIzguQvTV8ZzP10YQUCdn1sX-mX3I");
